477137d2d1d3117246e9f6b4a6c344082c0f8dbe
SaraSanchezL <sarasanchezlopez93@gmail.com>
2022-01-18 08:45:26 +0100

    add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.scss';
+import {useState, useEffect} from 'react';
+//import ls from '../services/localStorage'
+//import DataApi from '../services/api'
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
